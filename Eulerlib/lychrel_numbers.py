5806391540bf8af658b8ee77c1777d49314ef976
DarkEclipz <larsrotgers@gmail.com>
2020-10-17 16:19:35 +0200

    lychrel speed up

--- Eulerlib/lychrel_numbers.py
+++ Eulerlib/lychrel_numbers.py
@@ -1,19 +1,14 @@
 import eulerlib as lib
 
-
-def is_lychrel(k):
-    for _ in range(50):
-        k = k + lib.digits_to_int(lib.digits(k)[::-1])
-        if lib.is_palindrome(k):
-            return False
-    return True
-
-
 def count_lychrel(n):
     count = 0
-    for i in range(10, n):
-        if is_lychrel(i):
-            count += 1
+    for k in range(n):
+        for _ in range(50):
+            k = k + int(str(k)[::-1])
+            strk = str(k)
+            if strk == strk[::-1]:
+                break
+        count += 1
     return count
 
 
