6fb2b70026d27bcfb036768cf18da857651464fb
DarkEclipz <larsrotgers@gmail.com>
2020-10-17 16:13:24 +0200

    p55

---
+++ Eulerlib/lychrel_numbers.py
@@ -0,0 +1,21 @@
+import eulerlib as lib
+
+
+def is_lychrel(k):
+    for _ in range(50):
+        k = k + lib.digits_to_int(lib.digits(k)[::-1])
+        if lib.is_palindrome(k):
+            return False
+    return True
+
+
+def count_lychrel(n):
+    count = 0
+    for i in range(10, n):
+        if is_lychrel(i):
+            print(i)
+            count += 1
+    return count
+
+
+lib.time_it(lambda: count_lychrel(10000))
