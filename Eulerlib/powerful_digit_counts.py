e7109565f257db277cf1568866e95091454759ea
darkeclipz <larsrotgers@gmail.com>
2021-03-01 19:44:23 +0100

    powerful digit counts

---
+++ Eulerlib/powerful_digit_counts.py
@@ -0,0 +1,23 @@
+"""
+The 5-digit number, 16807=75, is also a fifth power. Similarly, the 9-digit number, 134217728=89, is 
+a ninth power.
+
+How many n-digit positive integers exist which are also an nth power?
+"""
+
+import eulerlib as euler
+import math
+
+def powerful_digit_counts():
+    count = 0
+    for n in range(1, 100):
+        lower = 10**(n - 1)
+        upper = 10**n
+        for k in range(1, 10):
+            if lower <= k**n < upper:
+                print('{}^{}={}'.format(k, n, k**n))
+                count += 1
+    return count
+
+euler.time_it(powerful_digit_counts)
+
