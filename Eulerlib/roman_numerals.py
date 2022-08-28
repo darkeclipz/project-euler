9c54c9189d1dd0009fa9ede9066cd4a573725cea
darkeclipz <larsrotgers@gmail.com>
2022-08-28 10:31:20 +0200

    refactoring of roman numerals

--- Eulerlib/roman_numerals.py
+++ Eulerlib/roman_numerals.py
@@ -13,53 +13,55 @@
 # X can only be placed before L and C.
 # C can only be placed before D and M.
 
-lookup = {
-    'I': 1,
-    'IV': 4,
-    'V': 5,
-    'IX': 9,
-    'X': 10,
-    'XL': 40,
-    'L': 50,
-    'XC': 90,
-    'C': 100,
-    'CD': 400,
-    'D': 500,
-    'CM': 900,
-    'M': 1000
-}
 
-inv_lookup = {v:k for k,v in lookup.items()}
 
 class RomanNumeral:
 
-    def __init__(self, x):
-        self.value = self.decode(x)
+    char_lookup = {
+        'I': 1,
+        'IV': 4,
+        'V': 5,
+        'IX': 9,
+        'X': 10,
+        'XL': 40,
+        'L': 50,
+        'XC': 90,
+        'C': 100,
+        'CD': 400,
+        'D': 500,
+        'CM': 900,
+        'M': 1000
+    }
 
-    def decode(self, x):
-        x = x.strip()
+    inv_char_lookup = {v:k for k,v in char_lookup.items()}
+    roman_numeral_chars = list(inv_char_lookup.keys())
+    roman_numeral_chars.sort(reverse = True)
+
+    def __init__(self, roman_numeral):
+        self.value = self.decode(roman_numeral)
+
+    def decode(self, roman_numeral):
+        roman_numeral = roman_numeral.strip()
         value = 0
         i = 0
-        while i < len(x):
-            if x[i:i+2] in lookup:
-                value += lookup[x[i:i+2]]
+        while i < len(roman_numeral):
+            if roman_numeral[i:i+2] in RomanNumeral.char_lookup:
+                value += RomanNumeral.char_lookup[roman_numeral[i:i+2]]
                 i += 2
-            elif x[i] in lookup:
-                value += lookup[x[i]]
+            elif roman_numeral[i] in RomanNumeral.char_lookup:
+                value += RomanNumeral.char_lookup[roman_numeral[i]]
                 i += 1
         return value
 
-    def encode(self, x):
-        rn = ""
-        values = [k for k,v in inv_lookup.items()]
-        values.sort(reverse = True)
-        while x > 0:
-            for i in values:
-                if i <= x:
-                    x -= i
-                    rn += inv_lookup[i]
+    def encode(self, decimal_number):
+        roman_numeral = ""
+        while decimal_number > 0:
+            for char in RomanNumeral.roman_numeral_chars:
+                if char <= decimal_number:
+                    decimal_number -= char
+                    roman_numeral += RomanNumeral.inv_char_lookup[char]
                     break
-        return rn
+        return roman_numeral
 
     def __repr__(self):
         return self.encode(self.value)
@@ -85,4 +87,3 @@ with open('roman_numerals.txt', 'r') as f:
         print("Reduced {} to {} saving {} characters.".format(line, str(rn), current_size - optimal_size))
     
 print(solution)
-        
